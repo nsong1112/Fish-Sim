5060cbf15eddfcdaba88d81fb2551418c2797d05
Nathan S <nathan.j.song@gmail.com>
2026-09-10 01:56:41 -0400

    Add tick system for tracking hours, step function for weather/sunlight

--- main.py
+++ main.py
@@ -2,19 +2,13 @@ from lake import Lake
 from lake_manager import LakeManager
 from fish import Fish
 from fish_school import FishSchool
+import pygame
 
-# 1. Initialize the Lake environment (This instantly builds all cells and layers!)
 my_lake = Lake('templates/test_lake.txt')
 my_lake_manager = LakeManager(my_lake,"va_seasonal_info.json")
 
-# 2. Let's verify the environment built correctly
-print("--- LAKE GENERATION REPORT ---")
-print(f"Lake Dimensions: {my_lake.width_x} cells wide by {my_lake.height_y} cells high.")
+clock = pygame.time.Clock()
 
-# 3. Test a shallow edge cell
-edge_cell = my_lake.grid[0][0]
-print(f"The cell at (0,0) is {edge_cell.max_depth}m deep and has {len(edge_cell.water_column)} layer(s).")
-
-# 4. Test the deep center cell
-center_cell = my_lake.grid[4][4]
-print(f"The cell at (4,4) is {center_cell.max_depth}m deep and has {len(center_cell.water_column)} layer(s).")
+while True:
+    my_lake_manager.step()
+    clock.tick(24) #24 hours per second
